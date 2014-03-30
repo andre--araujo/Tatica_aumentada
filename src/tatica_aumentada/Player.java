d65a9eddf12bb3931e875f46fbfefaee161d3b8e
andre--araujo <andrearaujo@poli.ufrj.br>
2014-03-29 21:04:28 -0300

    0.1
    Prévia da janela (java)

---
+++ src/tatica_aumentada/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tatica_aumentada;
+
+/**
+ *
+ * @author Andre
+ */
+public class Player {
+    
+    String name;
+    String picture; //nome do arquivo da foto de rosto
+    String card; // nome do arquivo do cartão
+    float shoulders;  // largura dos ombros
+    float height;   //altura
+    float waist;    //largura da cintura
+    int color;     // cor do bonequino
+}
---
+++ src/tatica_aumentada/Tatica_Aumentada.java
@@ -0,0 +1,16 @@
+package tatica_aumentada;
+
+/**
+ *
+ * @author Andre
+ */
+public class Tatica_Aumentada {
+
+   
+    public static void main(String[] args) {
+        
+        MainWindow mainWindow = new MainWindow();  //cria a janela
+        mainWindow.setVisible(true); //desenha a janela
+    }
+    
+}
